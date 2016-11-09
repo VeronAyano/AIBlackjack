046dbf1e006372bc73cd93d0fc6e272805a292e7
Vicente Reyes <vicentereyes@macbook-pro-de-vicente.local>
2016-11-08 19:27:39 -0600

    Initial Commit

---
+++ proyectoFinal/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  proyectoFinal
+//
+//  Created by Vicente Reyes on 11/8/16.
+//  Copyright © 2016 Vicente Reyes. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
