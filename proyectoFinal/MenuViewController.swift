bcf5d2a9ec249324e5a947fe25b2552106cc1069
Eliezer Hernandez <eliezerhernandez@macbook-pro-de-arkham.local>
2016-11-15 12:35:32 -0600

    hola

---
+++ proyectoFinal/MenuViewController.swift
@@ -0,0 +1,25 @@
+//
+//  MenuViewController.swift
+//  proyectoFinal
+//
+//  Created by Eliezer Hernandez on 15/11/16.
+//  Copyright © 2016 Vicente Reyes. All rights reserved.
+//
+
+import UIKit
+
+class MenuViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
+
