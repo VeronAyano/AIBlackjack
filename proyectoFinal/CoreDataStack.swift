62f5e8784d201c9952a93011635f6b1f1c5c2d01
Vicente Reyes <vicentereyes@macbook-pro-de-vicente.local>
2016-11-23 11:49:38 -0600

    Agregada funcionalidad para Core Data

---
+++ proyectoFinal/CoreDataStack.swift
@@ -0,0 +1,43 @@
+//
+//  CoreDataStack.swift
+//  proyectoFinal
+//
+//  Created by Vicente Reyes on 11/23/16.
+//  Copyright © 2016 Vicente Reyes. All rights reserved.
+//
+// Checar http://www.codebeaulieu.com/10/adding-core-data-using-swift-2 para mas informacion
+
+import UIKit
+import CoreData
+class CoreDataStack: NSObject {
+    
+    var managedObjectContext: NSManagedObjectContext
+    
+    override  init() {
+        // This resource is the same name as your xcdatamodeld contained in your project.
+        guard let modelURL = Bundle.main.url(forResource: "proyectoFinal", withExtension:"momd") else {
+            fatalError("Error loading model from bundle")
+        }
+        // The managed object model for the application. It is a fatal error for the application not to be able to find and load its model.
+        guard let mom = NSManagedObjectModel(contentsOf: modelURL) else {
+            fatalError("Error initializing mom from: \(modelURL)")
+        }
+        let psc = NSPersistentStoreCoordinator(managedObjectModel: mom)
+        self.managedObjectContext = NSManagedObjectContext(concurrencyType: .mainQueueConcurrencyType)
+        self.managedObjectContext.persistentStoreCoordinator = psc
+        
+        let urls = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        let docURL = urls[urls.endIndex-1]
+        /* The directory the application uses to store the Core Data store file.
+         This code uses a file named "DataModel.sqlite" in the application's documents directory.
+         */
+        //let storeURL = docURL.URLByAppendingPathComponent("Up_and_Running_With_Core_Data.sqlite")
+        let storeURL = docURL.appendingPathComponent("proyectoFinal.sqlite")
+        do {
+            try psc.addPersistentStore(ofType: NSSQLiteStoreType, configurationName: nil, at: storeURL, options: nil)
+        } catch {
+            fatalError("Error migrating store: \(error)")
+        }
+        
+    }
+}
