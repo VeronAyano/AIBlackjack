9347ee608357fac37b3bf49bfbd8100dec0a1d06
Eliezer Hernandez <eliezerhernandez@macbook-pro-de-arkham.local>
2016-11-23 19:13:28 -0600

    mapas

---
+++ proyectoFinal/MapViewController.swift
@@ -0,0 +1,47 @@
+//
+//  MapViewController.swift
+//  proyectoFinal
+//
+//  Created by Eliezer Hernandez on 23/11/16.
+//  Copyright © 2016 Vicente Reyes. All rights reserved.
+//
+
+import UIKit
+import MapKit
+import CoreLocation
+
+class MapViewController: UIViewController, CLLocationManagerDelegate{
+    
+    @IBOutlet var map: MKMapView!
+    
+    let manager = CLLocationManager()
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let location = locations[0]
+        let span:MKCoordinateSpan = MKCoordinateSpanMake(0.01, 0.01)
+        let myLocation: CLLocationCoordinate2D = CLLocationCoordinate2DMake(location.coordinate.latitude,location.coordinate.longitude)
+        let region:MKCoordinateRegion = MKCoordinateRegionMake(myLocation, span)
+        map.setRegion(region, animated: true)
+        
+        print(location.altitude)
+        print(location.speed)
+        self.map.showsUserLocation = true
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        manager.delegate = self
+        manager.desiredAccuracy = kCLLocationAccuracyBest
+        manager.requestWhenInUseAuthorization()
+        manager.startUpdatingLocation()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
+
